0269e44997048f714166b3d0d5db1f0c980179d5
Corinne <corinne.boesch@gmail.com>
2021-07-21 14:40:15 +0100

    Student Dierctory with menu

--- directory.rb
+++ directory.rb
@@ -1,17 +1,41 @@
-# first we print the list of students
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLage", cohort: :november}, 
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-] 
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+
+    selection = gets.chomp
+
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+def input_students
+  puts "Pleae enter the name of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
   puts "The students of Villans Academy"
@@ -24,11 +48,8 @@ def print(students)
   end
 end
 
-def print_footer(names)
-  puts  "Overall, we have: #{names.count} great students"
+def print_footer(students)
+  puts  "Overall, we have: #{students.count} great students"
 end
-# nothing happes until we call the methods
 
-print_header
-print(students)
-print_footer(students)
+interactive_menu
